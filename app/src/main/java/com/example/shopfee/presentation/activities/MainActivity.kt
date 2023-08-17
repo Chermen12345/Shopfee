620f3f796db482532eaf5bbbde5309b63db0fbd4
Chermen12345 <bekuzarovchermen@gmail.com>
2023-08-17 15:16:44 +0400

    setUp

--- app/src/main/java/com/example/shopfee/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/example/shopfee/presentation/activities/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.example.shopfee
+package com.example.shopfee.presentation.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.shopfee.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
     }
 
