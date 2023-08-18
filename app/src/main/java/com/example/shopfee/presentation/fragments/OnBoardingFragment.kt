e3939a32608f85b300993632ac36805a96af9361
Chermen12345 <bekuzarovchermen@gmail.com>
2023-08-18 19:51:48 +0400

    made onboarding fully

--- app/src/main/java/com/example/shopfee/presentation/fragments/OnBoardingFragment.kt
+++ app/src/main/java/com/example/shopfee/presentation/fragments/OnBoardingFragment.kt
@@ -1,10 +1,12 @@
 package com.example.shopfee.presentation.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.shopfee.R
 import com.example.shopfee.databinding.FragmentOnBoardingBinding
 import com.example.shopfee.presentation.adapters.ViewPagerOnBoarding
@@ -40,11 +42,34 @@ class OnBoardingFragment : Fragment() {
         adapterPager = ViewPagerOnBoarding(requireActivity().supportFragmentManager,lifecycle)
         setUpPagerAdapter()
 
+        goToLogin()
 
     }
 
     private fun setUpPagerAdapter(){
         binding.pager.adapter = adapterPager
     }
+
+
+    //get Preferences go to login or onBoarding
+
+    //here fun returns boolean which shows did user page all pages onBoarding or not
+    private fun isOnBoarding():Boolean{
+
+        val pref = activity?.getSharedPreferences("onBoarding",Context.MODE_PRIVATE)
+        val isGoLogin = pref?.getBoolean("IsGoToLogin",false)
+        return isGoLogin ?: return false
+    }
+
+    private fun goToLogin(){
+
+        if (isOnBoarding()){
+
+            findNavController().navigate(R.id.action_onBoardingFragment_to_loginFragment)
+        }
+    }
+
+
+
 
 }
--- app/src/main/java/com/example/shopfee/presentation/fragments/PageFragment2.kt
+++ app/src/main/java/com/example/shopfee/presentation/fragments/PageFragment2.kt
@@ -1,60 +1,73 @@
 package com.example.shopfee.presentation.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.viewpager2.widget.ViewPager2
 import com.example.shopfee.R
+import com.example.shopfee.databinding.FragmentPage2Binding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PageFragment2.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PageFragment2 : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+
+    private lateinit var binding: FragmentPage2Binding
+
+    var pager: ViewPager2 ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_page2, container, false)
+
+        binding = FragmentPage2Binding.inflate(inflater)
+        pager = activity?.findViewById(R.id.pager)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PageFragment2.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PageFragment2().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        goToPage3()
+
+        skipOnBoarding()
     }
+
+    //view paging from 2 to 3 fragment
+    private fun goToPage3(){
+
+        binding.btpage3.setOnClickListener {
+
+            pager?.currentItem = 3
+        }
+
+    }
+
+
+    //here if user clicks to skip on boarding he navigates to login fragment and we save
+    //and we save boolean with value = true to shared pref not to show onBoarding every time
+    //user opens the app
+    private fun skipOnBoarding(){
+
+        binding.btskiponboard.setOnClickListener {
+
+
+
+            findNavController().navigate(R.id.action_onBoardingFragment_to_loginFragment)
+        }
+
+    }
+
+
+
+
 }
